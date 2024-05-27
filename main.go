522b9993f713f96589fe49f9c8d9e2d2a200c3ea
anrikki666 <168280118+anrikki666@users.noreply.github.com>
2024-05-28 04:21:27 +0600

    Update main.go

--- main.go
+++ main.go
@@ -4,32 +4,46 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
-func fetchAndSaveFromAPI(apiURL, filename string) error {
-	resp, err := http.Get(apiURL)
+func fetchAndSave(url, filename string) error {
+	// Отправляем GET-запрос к указанному URL
+	resp, err := http.Get(url)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
+
+	// Проверяем код состояния ответа
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("не удалось получить данные: статус %d", resp.StatusCode)
+	}
+
+	// Считываем тело ответа
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return err
 	}
+
+	// Сохраняем тело ответа в файл
 	err = ioutil.WriteFile(filename, body, 0644)
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf("Ответ от API сохранен в файл: %s\n", filename)
+	fmt.Printf("Ответ от %s сохранен в файл: %s\n", url, filename)
 	return nil
 }
 
 func main() {
-	apiURL := "https://api.stormglass.io/v2"
-	filename := "main.go"
+	// URL, на который отправляем запрос
+	url := "https://cert.kz/"
+	// Имя файла, в который будет сохранен ответ
+	filename := "index.html"
 
-	err := fetchAndSaveFromAPI(apiURL, filename)
+	// Выполняем запрос и сохраняем ответ в файл
+	err := fetchAndSave(url, filename)
 	if err != nil {
 		fmt.Println("Ошибка:", err)
 		return
